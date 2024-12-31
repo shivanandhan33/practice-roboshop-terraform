03d491633b6ba5ad51c5f0cfc3ce21ab94b3a6ff
Shiva <shivanandhan33@gmail.com>
2024-12-31 12:50:15 +0530

    New repo

--- variables.tf
+++ variables.tf
@@ -2,8 +2,7 @@ variable "env"{}
 variable "db_instances"{}
 variable "app_instances"{}
 variable "web_instances"{}
-variable "app_port"{}
+# variable "app_port"{}
 variable "zone_id"{}
 variable "domain_name"{}
 variable "vault_token"{}
-variable "component_name"{}
