72499fda9f22edb3ef92abf4bb917463cafc5855
Shiva <shivanandhan33@gmail.com>
2024-12-31 21:46:01 +0530

    New repo

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "instance" {
       }
       inline = [
         "sudo labauto ansible",
-        "ansible-pull -i localhost, -U https://github.com/shivanandhan/Practice-roboshop-ansible roboshop.yml -e env=${var.env} -e app_name=${var.component_name} -e vault_token=${var.vault_token}"
+        "ansible-pull -i localhost, -U https://github.com/shivanandhan/Practice-roboshop-ansible roboshop.yml -e env=${var.env} -e component=${var.component_name} -e vault_token=${var.vault_token}"
       ]
     }
   }
--- variables.tf
+++ variables.tf
@@ -2,7 +2,6 @@ variable "env"{}
 variable "db_instances"{}
 variable "app_instances"{}
 variable "web_instances"{}
-# variable "app_port"{}
 variable "zone_id"{}
 variable "domain_name"{}
 variable "vault_token"{}
